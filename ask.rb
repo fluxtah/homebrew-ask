a6b95628dc779c880e1a7b49d966d275e4b0a161
Ian Warwick <ian.warwick@justeattakeaway.com>
2024-05-04 11:51:40 +0100

    fix checksum

--- ask.rb
+++ ask.rb
@@ -2,7 +2,7 @@ class Ask < Formula
   desc "A tool to access OpenAI assistants on the command line with easy way to define and interact with assistants."
   homepage "https://github.com/fluxtah/ask"
   url "https://github.com/fluxtah/ask/releases/download/v0.11/ask-0.11.tar.gz"
-  sha256 "1d608a322db95109bfedb3a9c379469d0eb7cb7e60599cf397014802f8c75e43 "
+  sha256 "95deae44e676ce8d9569b359dae62046ae657728d99babc92913fa5792d14c24"
 
   def install
     # Install the JAR file to the 'lib' directory
