c6acf04997963c5c42f2de145946decccad3721d
Ian Warwick <ian.warwick@justeattakeaway.com>
2024-05-13 14:07:18 +0100

    0.6.0

--- ask.rb
+++ ask.rb
@@ -1,9 +1,9 @@
 class Ask < Formula
   desc "A tool to access OpenAI assistants on the command line with easy way to define and interact with assistants."
   homepage "https://github.com/fluxtah/ask"
-  version "0.5.0"
+  version "0.6.0"
   url "https://github.com/fluxtah/ask/releases/download/v#{version}/ask-#{version}.tar.gz"
-  sha256 "28642e09103407c3d393b88e19cda5c62cc4e949373fe3f5237c9e71b3ce31da"
+  sha256 "8feddcc77c906e1473d8b5960b5a68c5bda0864acaf629b5a3d3d4bec502f819"
 
   def install
     jar_name = "ask-#{version}.jar"
@@ -16,6 +16,12 @@ class Ask < Formula
 
     # Modify the script to call the JAR from the correct location
     inreplace bin/"ask", "PATH_TO_JAR", "#{lib}/#{jar_name}"
+
+    # Create the .ask directory in the user's home directory
+    (Pathname.new("#{ENV["HOME"]}/.ask")).mkpath
+
+    # Extract and copy the plugins folder to the .ask directory
+    system "tar", "-xzf", "ask-#{version}.tar.gz", "plugins", "-C", "#{ENV["HOME"]}/.ask"
   end
 
   test do
