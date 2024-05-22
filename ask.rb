5b0cd37c1d50000473111031b51a7b5ed7135565
Ian Warwick <ian.warwick@justeattakeaway.com>
2024-05-22 19:46:34 +0100

    update

--- ask.rb
+++ ask.rb
@@ -1,9 +1,9 @@
 class Ask < Formula
   desc "A tool to access OpenAI assistants on the command line with easy way to define and interact with assistants."
   homepage "https://github.com/fluxtah/ask"
-  version "0.7.5"
+  version "0.7.7"
   url "https://github.com/fluxtah/ask/releases/download/v#{version}/ask-#{version}.tar.gz"
-  sha256 "fe182157ef5e104ea343912639efa3e043904f55c0d008bd566dec2c0381bd6f"
+  sha256 "81952d4b079f3666f9416faec85fd309851e67379f055a3df38a5de1236fc973"
 
   def install
     jar_name = "ask-#{version}.jar"
